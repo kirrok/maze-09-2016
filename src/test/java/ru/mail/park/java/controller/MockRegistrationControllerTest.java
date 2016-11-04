c1ddab71321cdb7be30d085d99f123b166975c1e
slavyanskii <mossbka@gmail.com>
2016-11-04 12:41:10 +0300

    test login

--- src/test/java/ru/mail/park/java/controller/MockRegistrationControllerTest.java
+++ src/test/java/ru/mail/park/java/controller/MockRegistrationControllerTest.java
@@ -3,29 +3,43 @@ package ru.mail.park.java.controller;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockHttpSession;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
-import ru.mail.park.services.impl.AccountServiceImpl;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 /**
  * Created by kirrok on 04.10.16.
  */
-@WebMvcTest
+@SpringBootTest
 @RunWith(SpringRunner.class)
+@AutoConfigureMockMvc
 public class MockRegistrationControllerTest {
-    @MockBean
-    private AccountServiceImpl accountService;
+    public MockHttpSession mockHttpSession;
 
     @Autowired
     private MockMvc mockMvc;
 
     @Test
     public void testRegistration() throws Exception {
-//        mockMvc.perform(post("/api/registration")
-//                .contentType(MediaType.APPLICATION_JSON)
-//                .content("{\"login\":\"kirrok\", \"password\":\"1234\", \"email\":\"some@email.ru\"}")
-//        ).andExpect(status().isOk()).andExpect(jsonPath("login").value("kirrok"));
+        mockMvc.perform(post("/registration")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content("{\"login\":\"login\",\"password\":\"test\"}")
+        ).andExpect(status().isOk());
+    }
+
+    @Test
+    public void testLogin() throws Exception {
+        mockHttpSession = new MockHttpSession();
+        mockMvc.perform(post("/auth")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content("{\"login\":\"login\",\"password\":\"test\"}")
+                .session(mockHttpSession)
+        ).andExpect(status().isOk());
     }
 }
